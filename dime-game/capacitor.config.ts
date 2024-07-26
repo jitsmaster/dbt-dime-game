e57f6014e2975206e69b607fde61cd80093f8cef
Arnold Wang <Arnold.Wang@ingeniux.com>
2024-07-26 23:19:54 +0000

    test push

--- dime-game/capacitor.config.ts
+++ dime-game/capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'dbt.dimegame',
-  appName: 'DimeGame',
-  webDir: 'dist'
+	appId: 'dbt.dimegame',
+	appName: 'DBT Dime Game',
+	webDir: 'dists'
 };
 
 export default config;
